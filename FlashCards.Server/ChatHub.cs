bc4e79f46efb11f2adb572ce2c305ace0e55afea
pride <szymekpaw5@gmail.com>
2023-10-25 21:48:03 +0200

    Added simple server, updated project references

---
+++ FlashCards.Server/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace FlashCards.Server
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            Console.WriteLine(message);
+            await Clients.All.SendAsync("MessageReceived", message);
+        }
+    }
+}
