100ff32dc6fffc91d6f2d367dff70af72c130cea
pride <szymekpaw5@gmail.com>
2023-10-29 13:37:33 +0100

    Renamed MainPage to RegisterPage

---
+++ FlashCards.Maui/Pages/RegisterPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using FlashCards.Contracts.Authentication;
+using FlashCards.Maui.Managers.Interfaces;
+
+namespace FlashCards.Maui.Pages;
+
+public partial class RegisterPage : ContentPage
+{
+    private IAuthenticationManager _authenticationManager;
+
+    public RegisterPage(IAuthenticationManager authenticationManager)
+    {
+        _authenticationManager = authenticationManager;
+        InitializeComponent();
+    }
+
+    private void OnRegisterButtonClicked(object sender, EventArgs e)
+    {
+        _authenticationManager.Register(new RegisterRequest(username.Text, email.Text, password.Text));
+    }
+}
